90053761103b74f76a1620f66475d58376e51f60
palpandi <palpandi.ramar@concertidc.com>
2025-09-27 09:33:00 +0530

    Remove element 179

--- 27.remove_element.rb
+++ 27.remove_element.rb
@@ -5,6 +5,7 @@ def remove_element(nums, val)
   k = 0
   nums.each_with_index do |n, i|
     if val != n
+      nums[i] = nums[k]
       nums[k] = n
       k += 1
     end
@@ -12,4 +13,7 @@ def remove_element(nums, val)
   k
 end
 
-p remove_element(nums, val)
+length = remove_element(nums, val)
+
+# Fill the remaining elements with '_'
+nums_filled = nums[0...length] + ['_'] * (nums.size - length)
